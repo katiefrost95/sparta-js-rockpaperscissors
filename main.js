a84a4854ee404e2c70495a58e967a83472ce17c8
Tech-A10 <tech-a10@Tech-A10s-MacBook-Air.local>
2017-11-01 11:00:38 +0000

    Get user input works

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var userInput = prompt ("Hello there! Are you ready to play? Pick either rock, paper, or scissors");
-var computerInput = Math.random ();
+var computerInput = Math.floor(Math.random()*3);
+
 console.log(computerInput);
 var playerScore = 0;
 var computerScore = 0;
@@ -7,47 +8,50 @@ var gameCounter = 0;
 var firstRound = true;
 var comp_pick;
 
-if (computerInput <= 0.33) {
+if (computerInput == 0) {
   comp_pick = "rock";
-} else if (0.33 < computerInput <= 0.67) {
+} else if (computerInput == 1) {
   comp_pick = "paper";
-} else if (0.67 < computerInput <= 1) {
+} else if (computerInput == 2) {
   comp_pick = "scissors";
 } else {
   comp_pick = "wrong";
 }
 
+
 decider(userInput, comp_pick);
 
 function decider (userInput, comp_pick) {
+// Loop after first round
   while (playerScore < 5 && computerScore < 5) {
+    computerInput = Math.floor(Math.random()*3)
     if (!firstRound) {
     userInput = prompt ("Lets go again, rock, paper, or scissors");
-    computerInput = Math.random ();
+    // computerInput = Math.random ();
     console.log(computerInput);
-    if (computerInput <= 0.33) {
+    if (computerInput == 0) {
       comp_pick = "rock";
-    } else if (0.33 < computerInput <= 0.67) {
+    } else if (computerInput == 1) {
       comp_pick = "paper";
-    } else if (0.67 < computerInput <= 1) {
+    } else if (computerInput == 2) {
       comp_pick = "scissors";
     } else {
       comp_pick = "wrong";
     }
   }
 // this is a draw
-  if ((comp_pick === "rock") && (userInput === "rock") || (userInput === "scissors" && comp_pick=== "scissors") || (userInput === "paper" && comp_pick === "paper")) {
+  if ((comp_pick === "rock" && userInput === "rock") || (userInput === "scissors" && comp_pick=== "scissors") || (userInput === "paper" && comp_pick === "paper")) {
   playerScore = playerScore + 1;
   computerScore = computerScore + 1;
   alert("I chose " + comp_pick + ". We draw! The score is " + playerScore + " / " + computerScore);
 }
 //This is a win
-  else if ((userInput === "rock") && (comp_pick === "scissors") || (userInput === "paper" && comp_pick === "rock") || (userInput === "scissors" && comp_pick === "paper")) {
+  else if ((userInput === "rock" && comp_pick === "scissors") || (userInput === "paper" && comp_pick === "rock") || (userInput === "scissors" && comp_pick === "paper")) {
     playerScore = playerScore + 1;
     alert("I chose " + comp_pick + ". You win this time! The score is " + playerScore + " / " + computerScore);
 }
 //This is a lose
-  else if ((userInput === "rock") && (comp_pick === "paper") || (userInput === "paper" && comp_pick === "scissors") || (userInput === "scissors" && comp_pick === "rock")) {
+  else if ((userInput === "rock" && comp_pick === "paper") || (userInput === "paper" && comp_pick === "scissors") || (userInput === "scissors" && comp_pick === "rock")) {
     computerScore = computerScore + 1;
     alert("I chose " + comp_pick + ". I win this time! The score is " + playerScore + " / " + computerScore);
 }
---
+++ solution/main.js
@@ -0,0 +1,15 @@
+// Get input
+
+function getUserInput() {
+  return prompt("Please chose either 'rock', 'paper', or 'scissors'.");
+}
+
+// Get the player move
+
+// Random play for computer
+
+// Get the computer move
+
+// Will need to play five
+
+// Check for a winner
