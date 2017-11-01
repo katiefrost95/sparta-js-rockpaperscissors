bf669e0bf2b52ecd11619715778b95794a932848
Tech-A10 <tech-a10@Tech-A10s-MacBook-Air.local>
2017-11-01 12:25:51 +0000

    fixed game

--- solution/main.js
+++ solution/main.js
@@ -1,29 +1,69 @@
 // Get input
-
 function getUserInput() {
-  return prompt("Please chose either 'rock', 'paper', or 'scissors'.");
+  return prompt("Please choose either 'rock', 'paper', or 'scissors'.");
 }
 
-// Get the player move
-function getPlayerMove () {
-  return getUserInput ();
-}
-
-// Random play for computer
-function randomPlay () {
+// Random play
+function randomPlay() {
   var randomNumber = Math.floor(Math.random() * 3);
   if (randomNumber === 0) {
-    return "Rock";
+    return "rock";
   } else if (randomNumber === 1) {
-    return "Paper";
+    return "paper";
   } else {
-    return "Scissors";
+    return "scissors";
   }
 }
 
+// get the player move
+function getPlayerMove(playerMove) {
+  return playerMove || getUserInput();
+}
 
-// Get the computer move
+// get the computer move
+function getComputerMove(computerMove) {
+  return computerMove || randomPlay();
+}
 
-// Will need to play five
+// will need to play to five
+function startGame() {
+  alert("Lets player Rock, Paper, Scissors");
+  // score variables
+  var playerScore = 0;
+  var computerScore = 0;
 
-// Check for a winner
+  // play to five
+  while(playerScore < 5 && computerScore < 5) {
+    var playerMove = getPlayerMove();
+    var computerMove = getComputerMove();
+    var winner = checkForWinner(playerMove, computerMove);
+
+    if (winner === "player"){
+        playerScore ++;
+        alert("The player won that round with - " + playerMove + "against - " + computerMove + "-------" + "The scores are --- Player " + playerScore + "Computer score --- " + computerScore);
+      } else if (winner === "computer"){
+        computerScore ++;
+        alert("The computer won that round with - " + computerMove + "against - " + playerMove + "-------" + "The scores are --- Player " + playerScore + "Computer score --- " + computerScore);
+      } else {
+        alert("That was a tie with - " + playerMove + computerMove)
+      }
+  }
+}
+
+// check for a winner
+function checkForWinner(playerMove, computerMove) {
+  var winner;
+
+  if (playerMove === computerMove){
+    winner = "tie";
+  } else if((playerMove === "rock" && computerMove === "scissors") || (playerMove === "paper" && computerMove === "rock") || (playerMove === "scissors" && computerMove === "paper")) {
+    winner = "player";
+  } else {
+    winner = "computer"
+  }
+
+  return winner;
+}
+
+
+startGame()
